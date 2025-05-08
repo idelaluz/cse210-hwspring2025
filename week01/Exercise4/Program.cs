058d9e2d38122d9242dd055d1620ec09499e4e4c
idelaluz <israel.delaluz@gmail.com>
2025-05-08 14:22:06 +0200

    Commit 08.05.2025 14h22

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to stop): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // Part 1: Calculate the sum
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum of the numbers is: {sum}");
+
+        //Part 2: Calculate the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the numbers is: {average}");
+
+        // Part 3: Find the maximum number
+
+        int maxNumber = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The maximum number is: {maxNumber}");
+
+        // Part 4: Find the minimum positive number
+        int minPositiveNumber = int.MaxValue;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && number < minPositiveNumber)
+            {
+                minPositiveNumber = number;
+            }
+        }
+        if (minPositiveNumber == int.MaxValue)
+        {
+            Console.WriteLine("There are no positive numbers in the list.");
+        }
+        else
+        {
+            Console.WriteLine($"The minimum positive number is: {minPositiveNumber}");
+        }
+
+
+        // Part 5: Sort the numbers in ascending order
+        numbers.Sort();
+        Console.WriteLine("The numbers in ascending order are: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
