1b13f7e1e663d33de362db8a08e9bf31bea9c20e
idelaluz <israel.delaluz@gmail.com>
2025-05-12 01:55:13 +0200

    Commit 12.05.2025 01h55

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -5,21 +5,30 @@ public class Journal
     public void AddEntry(Entry newEntry)
     {
         // Add the new entry to the list of entries
+        _entries.Add(newEntry);
     }
 
     public void DisplayAll()
     {
         // Display all entries in the journal entry list
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
         
     }
 
     public void SaveToFile(string file)
     {
         // Loop through each item in _entries and save them in a file
+
     }
 
     public void LoadFromFile(string file)
     {
         // Loop through each line of the file and create Entry objects to put in the _entries list
+
+        
     }
 }
