acf23f58b597c65e5af85dd397100f99260b3554
idelaluz <israel.delaluz@gmail.com>
2025-05-11 23:22:18 +0200

    Commit 11.05.2025 23h22

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine("Welcome to the Journal Program!");
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write a new entry");
+        Console.WriteLine("2. Display all entries");
+        Console.WriteLine("3. Save entries to file");
+        Console.WriteLine("4. Load entries from file");
+        Console.WriteLine("5. Exit");
+        Console.Write("Enter your choice: ");
     }
 }
